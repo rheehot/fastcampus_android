6efd342179f135ffe5a4253b82e3a299a3194106
peter L <peter.l@uvaper.com>
2019-08-04 16:37:36 +0900

    android study phonebook homework

---
+++ Android/14.phonebook/app/src/main/java/fast/campus/phonebook/PhoneBook.java
@@ -0,0 +1,14 @@
+package fast.campus.phonebook;
+
+public class PhoneBook {
+
+    public String image;
+    public String name;
+    public String number;
+
+    public PhoneBook(String image, String name, String number) {
+        this.image = image;
+        this.name = name;
+        this.number = number;
+    }
+}
