ab85debfa365590b3aa6a6a522cf310eab1ac06b
peter L <peter.l@uvaper.com>
2019-08-10 12:34:29 +0900

    android study shared preference

---
+++ Android/18.SharedPreference/app/src/main/java/fast/campus/sharedperference/Person.java
@@ -0,0 +1,11 @@
+package fast.campus.sharedperference;
+
+public class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
