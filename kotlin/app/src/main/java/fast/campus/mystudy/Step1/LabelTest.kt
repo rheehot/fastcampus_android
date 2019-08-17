c6d7f8f3e609aee229be4cc07558638299bb9020
peter L <peter.l@uvaper.com>
2019-08-17 17:35:23 +0900

    android kotlin study 08-17

---
+++ kotlin/app/src/main/java/fast/campus/mystudy/Step1/LabelTest.kt
@@ -0,0 +1,36 @@
+package fast.campus.mystudy.Step1
+
+import fast.campus.mystudy.TestClass
+
+class LabelTest (p : (Any) ->Unit ) : TestClass(p) {
+
+    override fun doTest() {
+        ExitLoop();
+        println(lambdaReturn())
+
+        // 함수형 스타일의 활용
+        (0..10).forEach {  if (it > 3) return@forEach else println(it) }
+    }
+
+    // @레이블 지정 후,  break@레이블로 나감
+// 대부분의 언어에서 지원하는 goto문과 유사함.
+    fun ExitLoop() {
+        HereToExit@ for(i in 0 .. 2){
+            for (j in  0.. 10){
+                if (j == 5) break@HereToExit;
+                kotlin.io.println("i->$i, j->$j")
+            }
+            kotlin.io.println("j loop end");
+        }
+        kotlin.io.println("i loop end");
+    }
+
+    // 이름없는 함수(람다식)의 리턴
+    var lambdaReturn = Exit@{
+        if(true){
+            return@Exit 3
+        }
+        1000
+    }
+}
+
