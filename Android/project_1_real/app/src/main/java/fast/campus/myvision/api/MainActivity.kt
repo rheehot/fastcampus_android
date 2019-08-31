5ae423facc90ae36b28d11bb2757f5b9af9347a1
peter L <peter.l@uvaper.com>
2019-08-31 17:39:34 +0900

    android app study 08-31 01

--- Android/project_1_real/app/src/main/java/fast/campus/myvision/api/MainActivity.kt
+++ Android/project_1_real/app/src/main/java/fast/campus/myvision/api/MainActivity.kt
@@ -2,11 +2,37 @@ package fast.campus.myvision.api
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupListener()
+    }
+
+    private fun setupListener(){
+
+      upload_image.setOnClickListener{
+
+//            UploadChooser().show(supportFragmentManager, "")
+            UploadChooser().apply {
+                addNotifier(object : UploadChooser.UploadChooseNotifierInterface{
+
+                    override fun cameraOnClick() {
+//                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                        Log.d("upload","cameraOnClick")
+                    }
+
+                    override fun gallaryOnClick() {
+                        Log.d("upload","gallaryOnClick")
+                    }
+                })
+            }.show(supportFragmentManager, "")
+        }
     }
 }
--- Android/project_1_real/app/src/main/java/fast/campus/myvision/api/UploadChooser.kt
+++ Android/project_1_real/app/src/main/java/fast/campus/myvision/api/UploadChooser.kt
@@ -1,3 +1,47 @@
 package fast.campus.myvision.api
 
-class UploadChooser
+import android.os.Bundle
+import android.support.design.widget.BottomSheetDialogFragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.upload_chooser.*
+
+class UploadChooser : BottomSheetDialogFragment(){
+
+    interface  UploadChooseNotifierInterface{
+        fun cameraOnClick()
+        fun gallaryOnClick()
+    }
+
+    var uploadChooserNotifierInterface : UploadChooseNotifierInterface? = null
+
+    fun addNotifier(listener : UploadChooseNotifierInterface) {
+        uploadChooserNotifierInterface = listener
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.upload_chooser, container, false)
+//        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setupListener()
+    }
+
+    private fun setupListener() {
+        upload_camera.setOnClickListener{
+            uploadChooserNotifierInterface?.cameraOnClick()
+            //권한 작업 가능
+        }
+
+        upload_gallary.setOnClickListener{
+         uploadChooserNotifierInterface?.gallaryOnClick()
+        }
+    }
+}
